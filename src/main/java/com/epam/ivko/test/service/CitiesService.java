d2de2cab897864076ed992a163b6de26ed48cf7d
Ivan Komissarov <zierg@yandex.ru>
2025-04-03 19:20:41 +0400

    Remove unnecessary SuppressWarnings

--- src/main/java/com/epam/ivko/test/service/CitiesService.java
+++ src/main/java/com/epam/ivko/test/service/CitiesService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Stream;
 
-@SuppressWarnings("unused")
 @Service
 public class CitiesService {
 
--- src/main/java/com/epam/ivko/test/storage/CitiesStorage.java
+++ src/main/java/com/epam/ivko/test/storage/CitiesStorage.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
-@SuppressWarnings("unused")
 @Service
 public class CitiesStorage {
 
