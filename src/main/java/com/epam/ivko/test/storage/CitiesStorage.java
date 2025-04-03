42ae4b33a4d8a7a9338e580b31d55739c74e7983
Ivan Komissarov <zierg@yandex.ru>
2025-04-03 14:19:07 +0400

    Add reading and writing to file

--- src/main/java/com/epam/ivko/test/storage/CitiesStorage.java
+++ src/main/java/com/epam/ivko/test/storage/CitiesStorage.java
@@ -1,6 +1,7 @@
 package com.epam.ivko.test.storage;
 
 import com.epam.ivko.test.storage.entity.City;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -11,19 +12,26 @@ public class CitiesStorage {
 
     private final CitiesCache cache;
     private final JsonPersistentStorage jsonStorage;
+    private final boolean persistChanges;
 
     public CitiesStorage(
             CitiesCache cache,
-            JsonPersistentStorage jsonStorage
+            JsonPersistentStorage jsonStorage,
+            @Value("${cities.persist-changes:#{false}}") boolean persistChanges
     ) {
         this.cache = cache;
         this.jsonStorage = jsonStorage;
+        this.persistChanges = persistChanges;
 
         loadCities();
     }
 
     public void addCity(City city) {
         cache.put(city);
+
+        if (persistChanges) {
+            jsonStorage.addCity(city);
+        }
     }
 
     public Set<City> getAll() {
--- src/main/java/com/epam/ivko/test/storage/JsonPersistentStorage.java
+++ src/main/java/com/epam/ivko/test/storage/JsonPersistentStorage.java
@@ -9,28 +9,71 @@ import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 @Component
 public class JsonPersistentStorage {
 
     private final String filePath;
+    private final String additionalCitiesFilePath;
+    private final ObjectMapper mapper;
 
     public JsonPersistentStorage(
-            @Value("${cities.source-file}") String filePath
+            @Value("${cities.source-file}") String filePath,
+            @Value("${cities.additional-source-file:#{null}}") String additionalCitiesFilePath,
+            ObjectMapper mapper
     ) {
         this.filePath = filePath;
+        this.additionalCitiesFilePath = additionalCitiesFilePath;
+        this.mapper = mapper;
+    }
+
+    public void addCity(City city) {
+        Set<City> cities = new HashSet<>(loadAdditionalCities());
+        cities.add(city);
+
+        try {
+            mapper.writeValue(new File(additionalCitiesFilePath), cities);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Set<City> getCities() {
-        return loadFileFromResources(filePath);
+        Set<City> cities = new HashSet<>(loadDefaultCities());
+        Set<City> additionalCities = loadAdditionalCities();
+
+        cities.addAll(additionalCities);
+        return cities;
     }
 
-    private Set<City> loadFileFromResources(String filePath) {
-        ObjectMapper mapper = new ObjectMapper();
+    private Set<City> loadAdditionalCities() {
+        if (additionalCitiesFilePath == null) {
+            return Collections.emptySet();
+        }
 
+        File file = new File(additionalCitiesFilePath);
+
+        if (!file.exists()) {
+            return Collections.emptySet();
+        }
+
+        return loadCitiesFromFile(file);
+    }
+
+    private Set<City> loadDefaultCities() {
         try {
             File file = ResourceUtils.getFile("classpath:" + filePath);
+            return loadCitiesFromFile(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Set<City> loadCitiesFromFile(File file) {
+        try {
             return mapper.readValue(file, new TypeReference<>() {});
         } catch (IOException e) {
             throw new RuntimeException(e);
